ed54498a09f78057bdd53ff80abbff8d7fba199a
contactochiga <contactochiga@gmail.com>
2025-08-22 16:40:45 +0100

    Update layout.js

--- ochiga-frontend/app/layout.js
+++ ochiga-frontend/app/layout.js
@@ -9,15 +9,11 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className="h-full">
-      <body className="flex flex-col min-h-screen bg-gray-50 dark:bg-gray-900">
-        {/* Top nav */}
+    <html lang="en">
+      <body className="relative min-h-screen pb-16">
+        {/* pb-16 = bottom padding so content doesn’t hide behind footer */}
         <NavBar />
-
-        {/* Main content with padding so footer doesn't overlap */}
-        <main className="flex-1 pb-16">{children}</main>
-
-        {/* Mobile bottom nav */}
+        <main className="pt-14">{children}</main>
         <Footer />
       </body>
     </html>
