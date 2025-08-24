213ff71c10f0d58fd1585c540246d61dbcb8c409
contactochiga <contactochiga@gmail.com>
2025-08-24 13:56:07 +0100

    Update layout.tsx

--- ochiga-frontend/src/app/layout.tsx
+++ ochiga-frontend/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
-import Header from '../components/Header'
-import Footer from '../components/Footer'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Navbar from './components/Navbar'
 import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
