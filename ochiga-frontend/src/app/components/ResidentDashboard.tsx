31311c46fceb2268e61a9baaeb4ea2086200555e
contactochiga <contactochiga@gmail.com>
2025-08-25 16:13:33 +0100

    Update ResidentDashboard.tsx

--- ochiga-frontend/src/app/components/ResidentDashboard.tsx
+++ ochiga-frontend/src/app/components/ResidentDashboard.tsx
@@ -1,1 +1,13 @@
+import TopBar from "@/components/TopBar";
+import Footer from "@/components/Footer";
+import ResidentDashboard from "@/components/ResidentDashboard";
 
+export default function DashboardPage() {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <TopBar />
+      <ResidentDashboard />
+      <Footer />
+    </div>
+  );
+}
