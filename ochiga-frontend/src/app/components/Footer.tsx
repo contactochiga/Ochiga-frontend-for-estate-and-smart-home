40c1f240e35f03ef2d37196aebbd4cacfe35d1e8
contactochiga <contactochiga@gmail.com>
2025-08-25 15:27:05 +0100

    Update Footer.tsx

--- ochiga-frontend/src/app/components/Footer.tsx
+++ ochiga-frontend/src/app/components/Footer.tsx
@@ -23,7 +23,7 @@ export default function Footer() {
 
   return (
     <footer
-      className="fixed bottom-0 left-0 right-0 z-50
+      className="w-full
         bg-white dark:bg-gray-800
         border-t border-gray-200 dark:border-gray-700 shadow-lg
         max-w-md mx-auto px-4
