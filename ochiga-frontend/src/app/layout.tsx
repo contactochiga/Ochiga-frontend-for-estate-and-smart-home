1639ca7d3c149be5b7c6694744aeb104e9548020
contactochiga <contactochiga@gmail.com>
2025-08-25 14:20:31 +0100

    Update layout.tsx

--- ochiga-frontend/src/app/layout.tsx
+++ ochiga-frontend/src/app/layout.tsx
@@ -18,7 +18,9 @@ export default function RootLayout({
     <html lang="en" className="h-full">
       <body className="flex flex-col min-h-screen relative z-0">
         <TopBar />
-        <main className="flex-1">{children}</main>
+        <main className="flex-1 pb-[calc(3.5rem+env(safe-area-inset-bottom))]">
+  {children}
+</main>
         <Footer />
       </body>
     </html>
