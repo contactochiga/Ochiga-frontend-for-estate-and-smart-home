129068c1aa7bb1168a73612846def8f641ccbb00
contactochiga <contactochiga@gmail.com>
2025-08-26 08:12:51 +0100

    Update postcss.config.js

--- ochiga-frontend/postcss.config.js
+++ ochiga-frontend/postcss.config.js
@@ -1,7 +1,7 @@
-// ochiga-frontend/postcss.config.js
-module.exports = {
-  plugins: {
-    "@tailwindcss/postcss": {},
-    autoprefixer: {},
-  },
-};
+export default function Home() {
+  return (
+    <h1 className="text-5xl font-bold text-red-600">
+      If this is BIG and RED, Tailwind is working ✅
+    </h1>
+  );
+}
