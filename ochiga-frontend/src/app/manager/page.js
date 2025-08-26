e09741dafd501d13502f95bbc11788e8e0680c28
contactochiga <contactochiga@gmail.com>
2025-08-26 13:57:50 +0000

    Create page.js

---
+++ ochiga-frontend/src/app/manager/page.js
@@ -0,0 +1,6 @@
+// src/app/manager/page.js
+import ManagerDashboard from "../manager-dashboard/page"; // <-- create this file later
+
+export default function Manager() {
+  return <ManagerDashboard />;
+}
