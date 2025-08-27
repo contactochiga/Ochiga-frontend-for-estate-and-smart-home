2c81a7af6e6d494ae96c009b48fd02c1011e1a95
contactochiga <contactochiga@gmail.com>
2025-08-27 07:06:12 +0100

    Update page.js

--- ochiga-frontend/src/app/page.js
+++ ochiga-frontend/src/app/page.js
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
-    <main className="min-h-screen bg-yellow-500 flex items-center justify-center">
-      <h1 className="text-3xl font-bold text-white">Tailwind Works 🎉</h1>
+    <main className="min-h-screen bg-red-600 flex items-center justify-center">
+      <h1 className="text-5xl font-bold text-green-300">
+        Tailwind Works 🎉
+      </h1>
     </main>
   );
 }
