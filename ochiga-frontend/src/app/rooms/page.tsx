f80da3a54c980473a59f621138b323c169222e67
Ochiga92 <96542292+Ochiga92@users.noreply.github.com>
2025-09-11 08:07:18 +0100

    Create page.tsx

---
+++ ochiga-frontend/src/app/rooms/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import RoomsDevices from "@/components/RoomsDevices";
+
+export default function RoomsPage() {
+  return (
+    <div className="p-4 pb-20">
+      <RoomsDevices />
+    </div>
+  );
+}
