ad04951ffa3fa2b8c927cda770e702451c34d0ec
contactochiga <contactochiga@gmail.com>
2025-09-25 17:08:33 +0100

    Update page.tsx

--- ochiga-frontend/src/app/manager-dashboard/page.tsx
+++ ochiga-frontend/src/app/manager-dashboard/page.tsx
@@ -1,13 +1,18 @@
-import PaymentsOverviewCard from "../../components/PaymentsOverviewCard";
+import ManagerHeaderCard from "../components/ManagerHeaderCard";
+import EstateStatsCard from "../components/EstateStatsCard";
+import CommunityFeedCard from "../components/CommunityFeedCard";
+import TasksCard from "../components/TasksCard";
+import PaymentsOverviewCard from "../components/PaymentsOverviewCard";
 
 export default function ManagerDashboardPage() {
   return (
     <div className="space-y-6 p-6">
+      {/* Manager Dashboard Layout */}
       <ManagerHeaderCard />
       <EstateStatsCard />
       <CommunityFeedCard />
       <TasksCard />
-      <PaymentsOverviewCard /> {/* 👈 Added here */}
+      <PaymentsOverviewCard /> {/* 👈 Payment card now included */}
     </div>
   );
 }
