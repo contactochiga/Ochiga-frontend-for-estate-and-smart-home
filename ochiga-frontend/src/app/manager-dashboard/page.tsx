38c80c86a2a9e611b36812823864148b1e366eea
contactochiga <contactochiga@gmail.com>
2025-09-25 16:49:13 +0100

    Update page.tsx

--- ochiga-frontend/src/app/manager-dashboard/page.tsx
+++ ochiga-frontend/src/app/manager-dashboard/page.tsx
@@ -1,13 +1,17 @@
-import ManagerHeaderCard from "../components/ManagerHeaderCard";
-import EstateStatsCard from "../components/EstateStatsCard";
-import CommunityFeedCard from "../components/CommunityFeedCard";
+"use client";
+
+import ManagerHeaderCard from "../../components/ManagerHeaderCard";
+import EstateStatsCard from "../../components/EstateStatsCard";
+import CommunityFeedCard from "../../components/CommunityFeedCard";
+import TasksCard from "../../components/TasksCard";
 
 export default function ManagerDashboardPage() {
   return (
-    <div className="p-6 space-y-6">
+    <div className="p-6 bg-gray-50 min-h-screen">
       <ManagerHeaderCard />
       <EstateStatsCard />
       <CommunityFeedCard />
+      <TasksCard />
     </div>
   );
 }
