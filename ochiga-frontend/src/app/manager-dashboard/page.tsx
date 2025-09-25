290c02f63c57ba29b32290670679a774a887651d
contactochiga <contactochiga@gmail.com>
2025-09-25 16:11:38 +0100

    Update page.tsx

--- ochiga-frontend/src/app/manager-dashboard/page.tsx
+++ ochiga-frontend/src/app/manager-dashboard/page.tsx
@@ -1,12 +1,18 @@
 "use client";
 
-import ManagerHeaderCard from "../components/ManagerHeaderCard";
-import EstateStatsCard from "../components/EstateStatsCard";
+import React from "react";
+import { ManagerHeaderCard, EstateStatsCard } from "../components";
+
 export default function ManagerDashboardPage() {
   return (
-    <div className="p-6 space-y-6">
+    <div className="min-h-screen bg-gray-50 dark:bg-gray-900 p-6 space-y-6">
+      {/* Header */}
       <ManagerHeaderCard />
+
+      {/* Estate Stats */}
       <EstateStatsCard />
+
+      {/* Add more cards below as we build out */}
     </div>
   );
 }
