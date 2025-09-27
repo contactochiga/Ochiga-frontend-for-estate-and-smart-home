416780015ccc83f2b199c707ff2e11205b4f9650
contactochiga <contactochiga@gmail.com>
2025-09-27 20:47:59 +0100

    Update index.ts

--- ochiga-frontend/src/app/dashboard/community/components/index.ts
+++ ochiga-frontend/src/app/dashboard/community/components/index.ts
@@ -1,4 +1,4 @@
 export { default as ComposerCard } from "./ComposerCard";
 export { default as PinnedPostCard } from "./PinnedPostCard";
 export { default as GroupsCard } from "./GroupsCard";
-export { default as FeedPostCard } from "./FeedPostCard";
+export { default as PostCard } from "./PostCard"; // ✅ added this line
