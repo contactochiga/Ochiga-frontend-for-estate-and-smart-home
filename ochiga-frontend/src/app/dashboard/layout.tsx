02a1bb0d45008067ae5d95308c1c3668318c4162
infopavnigeria-cpu <info.pavnigeria@gmail.com>
2025-11-01 11:14:16 +0100

    Update layout.tsx

--- ochiga-frontend/src/app/dashboard/layout.tsx
+++ ochiga-frontend/src/app/dashboard/layout.tsx
@@ -1,21 +1,17 @@
-// src/app/dashboard/layout.tsx
 "use client";
 
 import { ReactNode } from "react";
-import { DashboardProvider } from "../context/DashboardContext";
 import TopBar from "../components/TopBar";
 import Footer from "../components/Footer";
 
 export default function DashboardLayout({ children }: { children: ReactNode }) {
   return (
-    <DashboardProvider>
-      <div className="flex flex-col min-h-screen">
-        <TopBar />
-        <main className="flex-1 p-6 bg-gray-50 dark:bg-gray-900">
-          {children}
-        </main>
-        <Footer />
-      </div>
-    </DashboardProvider>
+    <div className="flex flex-col min-h-screen">
+      <TopBar />
+      <main className="flex-1 p-6 bg-gray-50 dark:bg-gray-900">
+        {children}
+      </main>
+      <Footer />
+    </div>
   );
 }
