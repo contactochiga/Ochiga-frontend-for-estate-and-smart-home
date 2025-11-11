b18cb919196c913c4225c5b4c98eb8c664872a36
Ochiga92 <96542292+Ochiga92@users.noreply.github.com>
2025-11-11 11:31:18 +0100

    Create index.ts

---
+++ ochiga-frontend/src/app/ai-dashboard/components/Panels/index.ts
@@ -0,0 +1,16 @@
+export { default as LightControl } from "./LightControl";
+export { default as WalletPanel } from "./WalletPanel";
+export { default as CCTVPanel } from "./CCTVPanel";
+export { default as EstatePanel } from "./EstatePanel";
+export { default as HomePanel } from "./HomePanel";
+export { default as RoomPanel } from "./RoomPanel";
+export { default as VisitorsPanel } from "./VisitorsPanel";
+export { default as PaymentsPanel } from "./PaymentsPanel";
+export { default as UtilitiesPanel } from "./UtilitiesPanel";
+export { default as CommunityPanel } from "./CommunityPanel";
+export { default as NotificationsPanel } from "./NotificationsPanel";
+export { default as HealthPanel } from "./HealthPanel";
+export { default as MessagePanel } from "./MessagePanel";
+export { default as IoTPanel } from "./IoTPanel";
+export { default as AiPanel } from "./AiPanel";
+export { default as AssistantPanel } from "./AssistantPanel";
