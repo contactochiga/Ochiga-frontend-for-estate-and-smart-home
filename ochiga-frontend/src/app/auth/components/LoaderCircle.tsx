98223a904793e527e074e358df3e340464f0478a
Ochiga92 <96542292+Ochiga92@users.noreply.github.com>
2025-11-12 17:01:43 +0100

    Create LoaderCircle.tsx

---
+++ ochiga-frontend/src/app/auth/components/LoaderCircle.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function LoaderCircle() {
+  return (
+    <div className="relative flex items-center justify-center">
+      <div className="w-12 h-12 border-4 border-emerald-500 border-t-transparent rounded-full animate-spin"></div>
+    </div>
+  );
+}
